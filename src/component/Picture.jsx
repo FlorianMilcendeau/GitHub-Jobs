3edd668f539eecf96e262dfe9e536012e66a1de9
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-18 00:41:44 +0200

    first commit

---
+++ src/component/Picture.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Picture.css"
+import PropTypes from "prop-types";
+
+const Picture = ({ className, src, alt }) => {
+  const [isComplete, setComplete] = useState(true);
+
+  const progressLoad = (e) => {
+    const img = e.target;
+    setComplete(img.complete);
+  };
+
+  return (
+    <div className="wrapper-picture">
+      {isComplete && (
+        <img
+          lazy="true"
+          onLoad={(e) => progressLoad(e)}
+          className={className}
+          src={
+            src
+              ? src
+              : "https://icon-library.com/images/photo-placeholder-icon/photo-placeholder-icon-17.jpg"
+          }
+          alt={alt}
+        />
+      )}
+      {isComplete === false && <div className="placeholder"></div>}
+    </div>
+  );
+};
+
+export default Picture;
+
+Picture.propTypes = {
+  className: PropTypes.string,
+  src: PropTypes.string,
+  alt: PropTypes.string.isRequired,
+};
