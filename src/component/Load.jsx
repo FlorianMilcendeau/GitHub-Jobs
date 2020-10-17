ed075505787ca2f3e60031f4b30e5cb6fb555e12
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-18 00:37:50 +0200

    first commit

---
+++ src/component/Load.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Load.css";
+
+const Load = () => (
+  <div className="ballWaiting">
+    <span className="ball"></span>
+    <span className="ball"></span>
+    <span className="ball"></span>
+  </div>
+);
+
+export default Load;
