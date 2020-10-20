592c9331d21f67fa076f7a089b215442f1aa72cf
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-20 22:50:50 +0200

    context added

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import Checkbox from "./Checkbox";
 import Input from "./Input";
 import earthIcon from "../assets/icon/earth.svg";
+import { SearchContext } from "../context/SearchContext";
 import "./SideBar.css";
 
 const SideBar = () => {
+  const { setLocation, setFullTime } = useContext(SearchContext);
+
   return (
     <aside className="SideBar">
-      <Checkbox id="fullTime" value="Full Time" />
+      <Checkbox getBool setChecked={(bool) => setFullTime(bool)} id="fullTime" value="Full Time" />
       <h3 className="title-minor">Location</h3>
-      <Input icon={earthIcon} placeHolder="City, state, zip code or country" />
-      <Checkbox id="london" value="London" />
-      <Checkbox id="amsterdam" value="Amsterdam" />
-      <Checkbox id="newYork" value="New York" />
-      <Checkbox id="Berlin" value="Berlin" />
+      <Input
+        setField={(val) => setLocation(val)}
+        icon={earthIcon}
+        placeHolder="City, state, zip code or country"
+      />
+      <Checkbox setChecked={(val) => setLocation(val)} id="london" value="London" />
+      <Checkbox setChecked={(val) => setLocation(val)} id="amsterdam" value="Amsterdam" />
+      <Checkbox setChecked={(val) => setLocation(val)} id="newYork" value="New York" />
+      <Checkbox setChecked={(val) => setLocation(val)} id="Berlin" value="Berlin" />
     </aside>
   );
 };
