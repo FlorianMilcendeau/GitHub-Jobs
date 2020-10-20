a2a904c9a7b002134d8d0485b8648ba23e0a17a2
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-20 22:46:06 +0200

    context added, a request is do when the location, fulltime or decription to change

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,27 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import CardJob from "./CardJob";
 import Pagination from "./Pagination";
-import { allJobs } from "../context/JobsContext";
+import { JobsContext } from "../context/JobsContext";
+import { SearchContext } from "../context/SearchContext";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Load from "./Load";
 
 const Main = () => {
-  const [jobs, setJobs] = useState([]);
+  const { description, location, fullTime } = useContext(SearchContext);
+  const allJobs = useContext(JobsContext);
   const [lengthPages, setLengthPages] = useState(0);
   const [cursor, SetCursor] = useState({ start: 0, end: 5 });
+  const [isLoad, setIsLoad] = useState(true);
 
   useEffect(() => {
+    setIsLoad(true);
     axios
-      .get("https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=1", {
-        header: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      })
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${description}&location=${location}&full_time=${fullTime}`,
+        {
+          header: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      )
       .then((res) => {
-        setLengthPages(res.data.length / 5);
+        const length = res.data.length;
+
+        if (length > 5) {
+          setLengthPages(Math.ceil(length / 5));
+        } else {
+          setLengthPages(0);
+        }
+        let i = 0,
+          array = [];
 
         for (const job of res.data) {
-          allJobs.push({
+          array.push({
+            id: job.id,
+            index: i,
             title: job.title,
             logo: job.company_logo,
             company: job.company,
@@ -31,17 +49,24 @@ const Main = () => {
             type: job.type,
             time: job.created_at,
           });
+
+          i++;
         }
 
-        setJobs(allJobs);
+        allJobs.setJobs(array);
+        SetCursor({ start: 0, end: length > 5 ? 5 : length });
+        setIsLoad(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [description, location, fullTime]);
 
   const nextPage = (pages, setPages, setCurrPage) => {
     const oldPage = document.querySelector(".current-page");
 
-    if (parseInt(oldPage.getAttribute("index")) < 10) {
+    window.scrollTo(0, document.querySelector(".Main").offsetTop);
+
+    if (parseInt(oldPage.getAttribute("index")) < lengthPages) {
       SetCursor({
         start: cursor.start + 5,
         end: cursor.end + 5,
@@ -68,6 +93,8 @@ const Main = () => {
   const prevPage = (pages, setPages, setCurrPage) => {
     const oldPage = document.querySelector(".current-page");
 
+    window.scrollTo(0, document.querySelector(".Main").offsetTop);
+
     if (parseInt(oldPage.textContent) === lengthPages - 2) {
       oldPage.nextElementSibling.textContent = "...";
     }
@@ -78,9 +105,17 @@ const Main = () => {
     ) {
       const [p1, p2, p3, dot] = pages;
       setPages([p1 - 1, p2 - 1, p3 - 1, dot]);
+
+      setCurrPage(parseInt(oldPage.getAttribute("index")) - 1);
+
+      SetCursor({
+        start: cursor.start - 5,
+        end: cursor.end - 5,
+      });
     }
 
     if (oldPage.previousElementSibling.classList.contains("prevPage") === false) {
+      setCurrPage(parseInt(oldPage.getAttribute("index")) - 1);
       setCurrPage(parseInt(oldPage.getAttribute("index")) - 1);
 
       SetCursor({
@@ -92,9 +127,15 @@ const Main = () => {
 
   return (
     <main className="Main">
-      {jobs.slice(cursor.start, cursor.end).map((job, index) => (
-        <Link to={`/${index}`} ><CardJob key={index} {...job} /></Link>
-      ))}
+      {isLoad ? (
+        <Load />
+      ) : (
+        allJobs.jobs.slice(cursor.start, cursor.end).map((job, index) => (
+          <Link key={job.id} to={`/${job.index}`}>
+            <CardJob index={job.index} {...job} />
+          </Link>
+        ))
+      )}
       {lengthPages > 0 && (
         <Pagination
           length={lengthPages}
