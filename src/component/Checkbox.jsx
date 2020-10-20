c0aed831354589de8c3654ed33f73e9871ba43a2
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-20 22:38:38 +0200

    'full time' option added, he receives a boolean

--- src/component/Checkbox.jsx
+++ src/component/Checkbox.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import './Checkbox.css'
+import React from "react";
+import "./Checkbox.css";
 import PropType from "prop-types";
+import { SearchContext } from "../context/SearchContext";
+import { useContext } from "react";
 
-const Checkbox = ({id, value}) => {
-    return (
-        <div className="wrapper-checkbox">
-            <input className="checkbox" id={id} name={id} type="checkbox"/> 
-            <label className="label-checkbox" htmlFor={id}>{value}</label>
-        </div>
-    )
-}
+const Checkbox = ({ id, value, setChecked, getBool }) => {
+  const { location, fullTime } = useContext(SearchContext);
+
+  const handleCheckbox = (e) => {
+    if (e.target.checked) {
+      setChecked(getBool ? true : id);
+    } else {
+      setChecked("");
+    }
+  };
+
+  return (
+    <div className="wrapper-checkbox">
+      {
+        <input
+          className="checkbox"
+          id={id}
+          name={id}
+          type="checkbox"
+          checked={location === id || fullTime === getBool ? true : false}
+          onChange={(e) => handleCheckbox(e)}
+        />
+      }
+      <label className="label-checkbox" htmlFor={id}>
+        {value}
+      </label>
+    </div>
+  );
+};
 
 export default Checkbox;
 
 Checkbox.propType = {
-    id: PropType.string.isRequired,
-    value: PropType.string.isRequired
-}
+  id: PropType.string.isRequired,
+  value: PropType.string.isRequired,
+  setChecked: PropType.func,
+};
