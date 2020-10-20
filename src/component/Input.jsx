8e2b72aad9734e7912773824094de0954c009697
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-20 22:43:29 +0200

    controlled fields added

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Input.css";
 import PropType from "prop-types";
 
-const Input = ({ placeHolder, icon }) => {
+const Input = ({ id, placeHolder, icon, setField, offLive }) => {
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    if (offLive !== true) {
+      if (typeof setField == "function") {
+        setField(value);
+      }
+    }
+  }, [value]);
+
+  const handleInput = async (e) => {
+    await setValue(e.target.value);
+  };
+
   return (
     <div className="wrapper-input">
       <img className="icon" src={icon} alt="icon" />
-      <input placeholder={placeHolder} type="search" className="Input" />
+      <input
+        id={id}
+        onChange={(e) => handleInput(e)}
+        value={value}
+        placeholder={placeHolder}
+        type="search"
+        className="Input"
+        onKeyPress={(e) => e.which === 13 && setField(value)}
+      />
     </div>
   );
 };
@@ -14,5 +36,7 @@ const Input = ({ placeHolder, icon }) => {
 export default Input;
 
 Input.propType = {
+  id: PropType.string,
   placeHolder: PropType.string,
+  setField: PropType.func,
 };
