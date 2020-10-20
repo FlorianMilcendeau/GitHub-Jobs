b87eea3bfc32ab73a8a62f0bf0e54603393719ee
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-10-20 22:40:34 +0200

    context added

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Header.css";
 import Input from "./Input";
 import Button from "./Button";
 import jobIcon from "../assets/icon/work.svg";
+import { SearchContext } from "../context/SearchContext";
 
 const Header = () => {
+  const { setDescription } = useContext(SearchContext);
   return (
     <header className="Header">
-      <Input icon={jobIcon} placeHolder="Title, companies, expertise or benefits" />
-      <Button value="search" />
+      <Input
+        offLive
+        id="search-description"
+        setField={(str) => setDescription(str)}
+        icon={jobIcon}
+        placeHolder="Title, companies, expertise or benefits"
+      />
+      <Button from="search-description" value="search" />
     </header>
   );
 };
