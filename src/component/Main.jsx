baa9ebb40c95babc0d639775eb84dc12c9db3b6e
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-11-05 21:34:14 +0100

    filter added to fulltime

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState, useContext } from "react";
-import CardJob from "./CardJob";
-import Pagination from "rc-pagination";
-import "rc-pagination/assets/index.css";
-import { JobsContext } from "../context/JobsContext";
-import { SearchContext } from "../context/SearchContext";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import Load from "./Load";
+import React, { useEffect, useState, useContext } from 'react';
+import CardJob from './CardJob';
+import Pagination from 'rc-pagination';
+import 'rc-pagination/assets/index.css';
+import { JobsContext } from '../context/JobsContext';
+import { SearchContext } from '../context/SearchContext';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Load from './Load';
 
 const Main = () => {
   const { description, location, fullTime } = useContext(SearchContext);
@@ -20,10 +20,10 @@ const Main = () => {
     setIsLoad(true);
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${description}&location=${location}&full_time=${fullTime}`,
+        `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${description}&location=${location}`,
         {
           header: {
-            "Access-Control-Allow-Origin": "*",
+            'Access-Control-Allow-Origin': '*',
           },
         }
       )
@@ -35,11 +35,9 @@ const Main = () => {
         } else {
           setLengthPages(0);
         }
-        let i = 0,
-          array = [];
 
-        for (const job of res.data) {
-          array.push({
+        const result = res.data.map((job, i) => {
+          return {
             id: job.id,
             index: i,
             title: job.title,
@@ -50,39 +48,52 @@ const Main = () => {
             apply: job.how_to_apply,
             type: job.type,
             time: job.created_at,
-          });
+          };
+        });
 
-          i++;
-        }
-
-        allJobs.setJobs(array);
+        allJobs.setJobs(result);
         SetCursor({ start: 0, end: length > 5 ? 5 : length });
         setIsLoad(false);
       })
       .catch((err) => console.log(err));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [description, location, fullTime]);
+  }, [description, location]);
 
   const onChange = (page, sizePages) => {
     setCurrentPage(page);
     SetCursor({ start: (page - 1) * sizePages, end: page * sizePages });
-    window.scrollTo(0, document.querySelector(".Main").offsetTop);
+    window.scrollTo(0, document.querySelector('.Main').offsetTop);
   };
 
   return (
-    <main className="Main">
+    <main className='Main'>
       {isLoad ? (
         <Load />
+      ) : allJobs.jobs.length ? (
+        allJobs.jobs
+          .filter((job) => {
+            if (fullTime) {
+              return 'Full Time' === job.type;
+            }
+            return job;
+          })
+          .slice(cursor.start, cursor.end)
+          .map((job, index) => (
+            <Link key={job.id} to={`/${job.index}`}>
+              <CardJob index={job.index} {...job} />
+            </Link>
+          ))
       ) : (
-        allJobs.jobs.length ?
-        allJobs.jobs.slice(cursor.start, cursor.end).map((job, index) => (
-          <Link key={job.id} to={`/${job.index}`}>
-            <CardJob index={job.index} {...job} />
-          </Link>
-        )) : <p className="none-job">no job offers here</p>
+        <p className='none-job'>no job offers here</p>
       )}
       {lengthPages > 0 && (
-        <Pagination style={{marginTop: "2rem"}} onChange={onChange} pageSize={5} current={currentPages} total={lengthPages} />
+        <Pagination
+          style={{ marginTop: '2rem' }}
+          onChange={onChange}
+          pageSize={5}
+          current={currentPages}
+          total={lengthPages}
+        />
       )}
     </main>
   );
