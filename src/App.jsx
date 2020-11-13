f6f0c92fae9c167003fbf94fa1c06b615f4f1c43
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-11-13 19:00:07 +0100

    context added for the location

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,46 @@
-import React, { lazy, Suspense } from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { JobsContextProvider } from "./context/JobsContext";
-import Load from "./component/Load";
-import { SearchContextProvider } from "./context/SearchContext";
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const Description = lazy(() => import("./root/Description"));
-const SearchJobs = lazy(() => import("./root/SearchJobs"));
+import Load from './component/Load';
+
+import { JobsContextProvider } from './context/JobsContext';
+import { SearchContextProvider } from './context/SearchContext';
+import { FilterContextProvider } from './context/FullTimeContext';
+import { LocationContextProvider } from './context/LocationContext';
+
+import './App.css';
+
+const Description = lazy(() => import('./root/Description'));
+const SearchJobs = lazy(() => import('./root/SearchJobs'));
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <h1>
           GitHub <span>Jobs</span>
         </h1>
         <Switch>
           <SearchContextProvider>
-            <JobsContextProvider>
-              <Suspense fallback={<Load />}>
-                <Route exact path="/" component={SearchJobs} />
-                <Route path="/:id" component={Description} />
-              </Suspense>
-            </JobsContextProvider>
+            <LocationContextProvider>
+              <JobsContextProvider>
+                <FilterContextProvider>
+                  <Suspense fallback={<Load />}>
+                    <Route exact path='/' component={SearchJobs} />
+                    <Route path='/:id' component={Description} />
+                  </Suspense>
+                </FilterContextProvider>
+              </JobsContextProvider>
+            </LocationContextProvider>
           </SearchContextProvider>
         </Switch>
         <footer>
           <address>
             <a
-              title="Profile Dev Challenges"
-              className="link-profil-challenge"
-              href="https://devchallenges.io/profile/VEssyN5qg9wX5gRmbjfz"
-              target="blank"
-            >
+              title='Profile Dev Challenges'
+              className='link-profil-challenge'
+              href='https://devchallenges.io/profile/VEssyN5qg9wX5gRmbjfz'
+              target='blank'>
               FlorianMilcendeau@devchallenges.io
             </a>
           </address>
